0f976e6beeb8c409149c67a51539f68513d03450
Manoj <83648898+DarkHunter1749@users.noreply.github.com>
2021-10-26 20:44:48 +0530

    Add files via upload

---
+++ task-15/Project Euler #1-#5 Problems Solutions/Project Euler #1- Multiples of 3 and 5.py
@@ -0,0 +1,10 @@
+def sum(x):
+    result = x*(x+1)//2
+    return result
+T = int(input())
+for i in range(T):
+    N = int(input())
+    a = (N-1) // 3
+    b = (N-1) // 5
+    c = (N-1) // 15
+    print(3*sum(a) + 5*sum(b) - 15*sum(c))
